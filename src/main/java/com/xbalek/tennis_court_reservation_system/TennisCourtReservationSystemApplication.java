a98b799cf090a82862f584b99051cc37ce503091
Filip Balek <xbalek@fi.muni.cz>
2024-07-21 23:42:04 +0200

    Added Application main

---
+++ src/main/java/com/xbalek/tennis_court_reservation_system/TennisCourtReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xbalek.tennis_court_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisCourtReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisCourtReservationSystemApplication.class, args);
+	}
+
+}
